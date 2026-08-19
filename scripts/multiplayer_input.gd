2fd91b37a649fdece16666f5462f85dd9c30f096
Kevin <koh705@mtroyal.ca>
2026-08-18 22:15:11 -0600

    added della glide

--- scripts/multiplayer_input.gd
+++ scripts/multiplayer_input.gd
@@ -22,8 +22,29 @@ func _physics_process(_delta):
 	
 	
 func _process(_delta):
+	
+	# Controls used by both players
 	if Input.is_action_just_pressed("jump") and player.movement_allowed:
 		jump.rpc()
+		
+	# Controls used by Della
+	if player.player_id != 1:
+		#if player.is_multiplayer_authority():
+		if Input.is_action_pressed("up"):
+			glide.rpc()
+		if Input.is_action_just_released("up"):
+			reset_glide.rpc()
+
+	
+@rpc("call_local")
+func glide():
+	if multiplayer.is_server() and player.velocity.y > 0:
+		player.gravity = player.GLIDE_GRAVITY
+		
+@rpc("call_local")
+func reset_glide():
+	if multiplayer.is_server():
+		player.gravity = player.DEFAULT_GRAVITY
 
 @rpc("call_local")
 func jump():
